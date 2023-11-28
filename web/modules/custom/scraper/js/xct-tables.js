21fbc4d584e4587f29c62a71761c0165e785caeb
Marco Gandi <elgandoz@gmail.com>
2023-11-28 12:08:47 +0100

    Fix relative time

--- web/modules/custom/scraper/js/xct-tables.js
+++ web/modules/custom/scraper/js/xct-tables.js
@@ -9,32 +9,45 @@ document.addEventListener("DOMContentLoaded", function () {
 
     document.querySelectorAll('.xct-flights td.xct-flights-date time').forEach(function (element) {
       const flightDate = new Date(element.getAttribute('datetime'));
-      const timeDifference = currentDate - flightDate;
+
+      // Reset the time portion of the dates to midnight
+      const currentDateMidnight = new Date(currentDate);
+      currentDateMidnight.setHours(0, 0, 0, 0);
+
+      const flightDateMidnight = new Date(flightDate);
+      flightDateMidnight.setHours(0, 0, 0, 0);
+
+      const timeDifference = currentDateMidnight - flightDateMidnight;
 
       let relativeTime;
       const dayOfWeek = formatDate(flightDate);
 
-      if (timeDifference < 24 * 60 * 60 * 1000) {
-        // Within the last 24 hours
+      if (timeDifference === 0) {
+        // Within the same calendar day
         relativeTime = 'Oggi';
-      } else if (timeDifference < 2 * 24 * 60 * 60 * 1000) {
+      } else if (timeDifference === 24 * 60 * 60 * 1000) {
         // Yesterday
         relativeTime = 'Ieri';
       } else if (timeDifference < 7 * 24 * 60 * 60 * 1000) {
         // Within the last week
-        relativeTime = `${dayOfWeek} scorso`;
+        relativeTime = `${dayOfWeek} ${dayOfWeek === 'domenica' ? 'scorsa' : 'scorso'}`;
       } else if (timeDifference < 14 * 24 * 60 * 60 * 1000) {
         // Within the last 2 weeks
-        const weeksAgo = Math.floor(timeDifference / (7 * 24 * 60 * 60 * 1000));
-        relativeTime = `${dayOfWeek}, ${weeksAgo} settimana fa`;
+        relativeTime = `${dayOfWeek}, una settimana fa`;
       } else if (timeDifference < 30 * 24 * 60 * 60 * 1000) {
-         // Within the last 2 weeks
-         const weeksAgo = Math.floor(timeDifference / (7 * 24 * 60 * 60 * 1000));
-         relativeTime = `${dayOfWeek}, ${weeksAgo} settimane fa`;
-      } else {
-        // More than a month ago
+        // Within the last month
+        const weeksAgo = Math.floor(timeDifference / (7 * 24 * 60 * 60 * 1000));
+        relativeTime = `${dayOfWeek}, ${weeksAgo} settimane fa`;
+      } else if (timeDifference < 365 * 24 * 60 * 60 * 1000) {
+        // Within the last year
         const monthsAgo = Math.floor(timeDifference / (30 * 24 * 60 * 60 * 1000));
-        relativeTime = `${dayOfWeek}, ${monthsAgo} mesi fa`;
+        relativeTime = `${monthsAgo} ${monthsAgo == 1 ? 'mese' : 'mesi'} fa`;
+      } else {
+        // More than a year ago
+        const yearsAgo = Math.floor(timeDifference / (365 * 24 * 60 * 60 * 1000));
+        const monthsAgo = Math.floor((timeDifference % (365 * 24 * 60 * 60 * 1000)) / (30 * 24 * 60 * 60 * 1000));
+        relativeTime = `${yearsAgo} ${yearsAgo == 1 ? 'anno' : 'anni'}`
+        relativeTime += monthsAgo > 0 ? ` e ${monthsAgo} ${monthsAgo == 1 ? 'mese' : 'mesi'} fa` : '';
       }
 
       // Clear existing content and create a new <em> element
