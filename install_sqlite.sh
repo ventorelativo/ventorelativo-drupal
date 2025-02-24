1d3bec58f68ce0f13d19ce1bed2933fc25b6ff54
Marco Gandi <elgandoz@gmail.com>
2025-02-24 20:13:40 +0100

    Netlify: another workaround for Sqlite3

--- install_sqlite.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-wget https://www.sqlite.org/2024/sqlite-autoconf-3450000.tar.gz
-tar xvf sqlite-autoconf-3450000.tar.gz
-cd sqlite-autoconf-3450000
-./configure --prefix=$HOME/sqlite
-make && make install
-export PATH=$HOME/sqlite/bin:$PATH
-sqlite3 --version
