76838115a0a7ae28d2a611ab98e3143b8ab282d9
marcogandi <m.gandi@iqual.ch>
2026-06-04 11:47:32 +0200

    Remove log

--- web/modules/custom/helper/js/theme-toggle.js
+++ web/modules/custom/helper/js/theme-toggle.js
@@ -3,7 +3,6 @@
  * Copyright 2011-2024 The Bootstrap Authors
  * Licensed under the Creative Commons Attribution 3.0 Unported License.
  */
-console.log('theme-toggle.js loaded');
 
 (() => {
   'use strict'
